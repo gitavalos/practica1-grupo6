b492bfe286d47d352544955ec02b74df21770394
Antonio Avalos <201114339@ingenieria.usac.edu.gt>
2018-02-21 23:01:46 -0600

    correccion

---
+++ Practica1/src/practica1/RaizCuadrada.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author Hugo
+ */
+public class RaizCuadrada {
+    
+    public static double RaizCuadrada(double num1)
+    {
+        
+        return Math.sqrt(num1);
+    }
+}
