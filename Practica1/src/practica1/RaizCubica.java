7bed6bd1755e1441a44563dba8968c292de3baf6
kevin4991 <keec.4991@gmail.com>
2018-02-22 22:37:33 -0600

    201403935 Funcionalidad Raiz Cubica

---
+++ Practica1/src/practica1/RaizCubica.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author NVIDIA
+ */
+public class RaizCubica {
+
+    public static double RaizCubica(double num1) {
+        return Math.pow(num1, 1 / 3.0);
+    }
+
+}
